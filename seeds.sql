9d89090753b5aef4bf6e3e93e331fb78cb204c20
Jennifer Stokes <jenniferstokes@Jennifers-MacBook-Pro.local>
2018-03-03 14:32:56 -0800

    added seeds

---
+++ seeds.sql
@@ -0,0 +1,6 @@
+USE pictionary;
+
+INSERT INTO users(user_name, high_score)VALUES('Jennifer', 15);
+INSERT INTO users(user_name, high_score)VALUES('Nathan', 12);
+INSERT INTO users(user_name, high_score)VALUES('Steven', 14);
+INSERT INTO users(user_name, high_score)VALUES('Michael', 12);
